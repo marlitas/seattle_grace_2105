bdc8b16242d088bc92d0e62f5bff64ce3e8c1803
Marla Schulz <marlaa.schulz@gmail.com>
2021-05-26 09:30:22 -0600

    Complete hospital spec and class

---
+++ lib/hospital.rb
@@ -0,0 +1,27 @@
+class Hospital
+  attr_reader :name, :chief_of_surgery, :doctors
+  def initialize(name, chief_of_surgery, doctors)
+    @name = name
+    @chief_of_surgery = chief_of_surgery
+    @doctors = doctors
+  end
+
+  def total_salary
+    doctors.sum do |doctor|
+      doctor.salary
+    end
+  end
+
+  def lowest_paid_doctor
+    lowest_paid = doctors.min_by do |doctor|
+      doctor.salary
+    end
+    lowest_paid.name
+  end
+
+  def specialties
+    doctors.map do |doctor|
+      doctor.specialty
+    end
+  end
+end
---
+++ spec/hospital_spec.rb
@@ -0,0 +1,47 @@
+require './lib/doctor'
+require './lib/hospital'
+
+RSpec.describe Hospital do
+
+  it 'exists' do
+    meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+    alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+    seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+
+    expect(seattle_grace).to be_a(Hospital)
+  end
+
+  it 'has attributes' do
+    meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+    alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+    seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+
+    expect(seattle_grace.name).to eq('Seattle Grace')
+    expect(seattle_grace.chief_of_surgery).to eq('Richard Webber')
+    expect(seattle_grace.doctors).to eq([meredith, alex])
+  end
+
+  it 'can calculate total salary' do
+    meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+    alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+    seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+
+    expect(seattle_grace.total_salary).to eq(190000)
+  end
+
+  it 'can find the lowest paid doctor' do
+    meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+    alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+    seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+
+    expect(seattle_grace.lowest_paid_doctor).to eq('Alex Karev')
+  end
+
+  it 'can store specialties' do
+    meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+    alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+    seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+
+    expect(seattle_grace.specialties).to eq(['General Surgery', 'Pediatric Surgery'])
+  end
+end
