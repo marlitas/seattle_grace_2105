decb4ed664ea7504e207a84ff2886e18007e0e53
Marla Schulz <marlaa.schulz@gmail.com>
2021-05-26 09:10:03 -0600

    Complete doctor spec and class

---
+++ spec/doctor_spec.rb
@@ -0,0 +1,19 @@
+require './lib/doctor'
+
+RSpec.describe do
+
+  it 'exists' do
+    meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+
+    expect(meredith).to be_a(Doctor)
+  end
+
+  it 'has attributes' do
+    meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+
+    expect(meredith.name).to eq('Meredith Grey')
+    expect(meredith.specialty).to eq('General Surgery')
+    expect(meredith.education).to eq('Harvard University')
+    expect(meredith.salary).to eq(100000)
+  end
+end
